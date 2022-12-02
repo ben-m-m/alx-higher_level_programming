20398103ebdb70ea247c3696c2d559b937403acd
ben-m-m <mainaben23@gmail.com>
2022-12-02 00:45:51 -0800

    done

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for lower in range(97, 123):
-    if chr(lower) is not 'q' and chr(lower) is not 'e':
+    if chr(lower) != 'q' and chr(lower) != 'e':
         print("{}".format(chr(lower)), end="")
