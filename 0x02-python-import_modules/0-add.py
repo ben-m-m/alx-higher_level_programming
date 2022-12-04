35b93d84345ee4af1971702c7d2cccb291ce8945
ben-m-m <mainaben23@gmail.com>
2022-12-04 03:49:53 -0800

    add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if _name_ == "_main_":
+    from _add_0_ import add
+
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+ Python learning about import and modules.
