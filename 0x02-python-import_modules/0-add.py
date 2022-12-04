ef6adccd1c71194c88fcf95f336d9197275f5f99
ben-m-m <mainaben23@gmail.com>
2022-12-04 03:55:34 -0800

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if _name_ == "_main_":
     from _add_0_ import add
-
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
