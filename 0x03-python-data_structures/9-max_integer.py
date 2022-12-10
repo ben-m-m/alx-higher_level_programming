497ad541308ab3beede522a2c7906ea5b4628f69
ben-m-m <mainaben23@gmail.com>
2022-12-09 20:51:23 -0800

    max integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function that finds the biggest integer of a list.
+"""
+
+
+def max_integer(my_list=[]):
+    if len(my_list) == "":
+        return None
+
+    large = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > large:
+            large = my_list[i]
+    return large
