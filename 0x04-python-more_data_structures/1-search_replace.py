117367d3f7cae24845de249f4f1f13d988d5f062
ben-m-m <mainaben23@gmail.com>
2022-12-12 00:22:29 -0800

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,7 +6,7 @@ of an element by another in a new list.
 
 
 def search_replace(my_list, search, replace):
-    new_list = my_list[]
+    new_list = my_list[:]
     for i in range(len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace
