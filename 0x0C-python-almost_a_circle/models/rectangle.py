46bba1670533e74c1b3baa8d918b002a1d814504
ben-m-m <mainaben23@gmail.com>
2023-01-20 16:10:26 +0000

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -116,8 +116,8 @@ class Rectangle(Base):
             - 4th argument should be the x attribute
             - 5th argument should be the y attribute
         """
-         """Update the object with keyword-argument"""
-         if args and len(args) != 0:
+        """Update the object with keyword-argument"""
+        if args and len(args) != 0:
             a = 0
             for arg in args:
                 if a == 0:
@@ -134,3 +134,19 @@ class Rectangle(Base):
                 elif a == 4:
                     self.y = arg
                 a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
