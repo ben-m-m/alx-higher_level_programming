694139b73ef1a2cbbb190c8b094b696cafa21b3c
ben-m-m <mainaben23@gmail.com>
2023-04-18 07:52:01 +0000

    to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,3 +20,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        list_dictionaries - is a list of dictionaries
+        Returns: JSON string representation of list_dictionaries
+                otherwise returns "[]" if None or empty
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
