9e4f67abf77503194422cbba1e4c491d7d848d49
ben-m-m <mainaben23@gmail.com>
2023-06-12 20:40:51 +0300

    second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const myVar = process.argv.length;
-if (myVar <= 3) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const args = process.argv.slice(2).map(x => parseInt(x));
-  const sorted = args.sort((a, b) => a < b);
-  console.log(sorted[1]);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
