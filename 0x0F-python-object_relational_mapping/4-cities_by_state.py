94f487cee0a14cc250aa8cd688c4269010f7a1be
ben-m-m <mainaben23@gmail.com>
2023-06-18 22:05:17 +0300

    lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,7 +6,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQldb.connect(
+    db = MySQLdb.connect(
             host="localhost", port=3306, user=argv[1],
             passwd=argv[2], database=argv[3])
 
