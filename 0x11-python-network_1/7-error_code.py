2a92dfdcb14aace2718bd64fa3ed10ca8affd8e2
ben-m-m <mainaben23@gmail.com>
2023-07-01 18:58:02 +0300

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    url = arg[1]
+    url = argv[1]
     r = requests.get(url)
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
