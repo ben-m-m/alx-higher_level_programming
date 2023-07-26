032a98b9c74ea1571cc2cb2d046b56a67ac68cb7
ben-m-m <mainaben23@gmail.com>
2023-07-26 19:57:59 +0300

    corr

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,9 +7,9 @@ request(url, (err, response, body) => {
   if (err) {
     console.log(err);
   }
-  const completed = {};
+  const completed = { };
   const tasks = JSON.parse(body);
-  for (let i in tasks) {
+  for (const i in tasks) {
     const task = tasks[i];
     if (task.completed === true) {
       if (completed[task.userId] === undefined) {
