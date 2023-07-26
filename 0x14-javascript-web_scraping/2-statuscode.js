c5222f67be183d80cc0adeeb2e775a1a878c30c5
ben-m-m <mainaben23@gmail.com>
2023-07-26 15:13:50 +0300

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const URL = process.argv[2];
+
+request(URL, (err, response) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('code: ' + response.statusCode);
+});
